630d7866c434c9ebd97eea7684448b9076e17c2c
zyxelva <zyxelva@gmail.com>
2018-06-19 11:02:46 +0800

    增加sweetalert.js，支持swal 弹窗； 增加 ignore 屏蔽无需上传的目录配置信息；

--- src/main/java/springboot/controller/admin/AuthController.java
+++ src/main/java/springboot/controller/admin/AuthController.java
@@ -1,10 +1,23 @@
 package springboot.controller.admin;
 
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import springboot.constant.WebConst;
 import springboot.controller.AbstractController;
 import springboot.controller.helper.ExceptionHelper;
@@ -17,13 +30,6 @@ import springboot.service.IUserService;
 import springboot.util.Commons;
 import springboot.util.MyUtils;
 
-import javax.annotation.Resource;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
 /**
  * 登录控制
  *
@@ -34,55 +40,55 @@ import java.io.IOException;
 @RequestMapping("/admin")
 @Transactional(rollbackFor = TipException.class)
 public class AuthController extends AbstractController {
-    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    @Resource
-    private IUserService userService;
+	@Resource
+	private IUserService userService;
 
-    @Resource
-    private ILogService logService;
+	@Resource
+	private ILogService logService;
 
-    @GetMapping(value = "/login")
-    public String login() {
-        return "admin/login";
-    }
+	@GetMapping(value = "/login")
+	public String login() {
+		return "admin/login";
+	}
 
-    @PostMapping(value = "login")
-    @ResponseBody
-    public RestResponseBo doLogin(@RequestParam String username,
-                                  @RequestParam String password,
-                                  HttpServletRequest request,
-                                  HttpServletResponse response) {
-        Integer error_count = cache.get("login_error_count");
-        try {
-            UserVo userVo = userService.login(username, password);
-            request.getSession().setAttribute(WebConst.LOGIN_SESSION_KEY, userVo);
-            // 设置12小时的cookie
-            MyUtils.setCookie(response, userVo.getUid());
-            logService.insertLog(LogActions.LOGIN.getAction(), null, request.getRemoteAddr(), userVo.getUid());
-        } catch (Exception e) {
-            error_count = null == error_count ? 1 : error_count + 1;
-            if (error_count > 3) {
-                return RestResponseBo.fail("您输入密码已经错误超过3次，请10分钟后尝试");
-            }
-            cache.set("login_error_count", error_count, 10 * 60);
-            String msg = "登录失败";
-            return ExceptionHelper.handlerException(logger, msg, e);
-        }
-        return RestResponseBo.ok();
-    }
+	@PostMapping(value = "login")
+	@ResponseBody
+	public RestResponseBo doLogin(@RequestParam String username,
+			@RequestParam String password,
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		Integer error_count = cache.get("login_error_count");
+		try {
+			final UserVo userVo = userService.login(username, password);
+			request.getSession().setAttribute(WebConst.LOGIN_SESSION_KEY, userVo);
+			// 设置12小时的cookie
+			MyUtils.setCookie(response, userVo.getUid());
+			logService.insertLog(LogActions.LOGIN.getAction(), null, request.getRemoteAddr(), userVo.getUid());
+		} catch (final Exception e) {
+			error_count = null == error_count ? 1 : error_count + 1;
+			if (error_count > 5) {
+				return RestResponseBo.fail("您输入密码已经错误超过5次，请10分钟后尝试");
+			}
+			cache.set("login_error_count", error_count, 10 * 60);
+			final String msg = "登录失败";
+			return ExceptionHelper.handlerException(AuthController.logger, msg, e);
+		}
+		return RestResponseBo.ok();
+	}
 
-    @RequestMapping("/logout")
-    public void logout(HttpSession session, HttpServletResponse response, HttpServletRequest request) {
-        session.removeAttribute(WebConst.LOGIN_SESSION_KEY);
-        Cookie cookie = new Cookie(WebConst.USER_IN_COOKIE, "");
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-        try {
-            response.sendRedirect(Commons.site_login());
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.error("注销失败", e);
-        }
-    }
+	@RequestMapping("/logout")
+	public void logout(HttpSession session, HttpServletResponse response, HttpServletRequest request) {
+		session.removeAttribute(WebConst.LOGIN_SESSION_KEY);
+		final Cookie cookie = new Cookie(WebConst.USER_IN_COOKIE, "");
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+		try {
+			response.sendRedirect(Commons.site_login());
+		} catch (final IOException e) {
+			e.printStackTrace();
+			AuthController.logger.error("注销失败", e);
+		}
+	}
 }
